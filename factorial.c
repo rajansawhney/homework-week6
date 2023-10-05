e1cd930caffea928007dd9d2c989f872d5fa060a
Rajan Sawhney <11162679+rajansawhney@users.noreply.github.com>
2023-10-04 21:09:29 -0700

    Initial commit

---
+++ factorial.c
@@ -0,0 +1,15 @@
+/*
+Write a C program that finds the factorial of a number, provided by the user, using recursion (the function calls itself).
+
+
+NOTE:
+1. Accept positive integers only
+2. MUST use a recursive function
+3. Display result in the following format ONLY:
+   Factorial of <number> = <factorial of number>
+
+Example:
+Enter a positive integer: 6
+Factorial of 6 = 720
+*/
+
---
+++ reverse.c
@@ -0,0 +1,16 @@
+/*
+Write a C program that reverses a sentence, provided by the user, using recursion (the function calls itself).
+
+NOTE:
+1. MUST use a recursive function
+2. If reading individual characters:
+   - use getchar() instead of scanf() - or you might run into segmentation faults with auto-grading
+   - to end recursion, check if the character is EOF (end of file character) - or you might run into segmentation faults with auto-grading
+3. Display just the reverse of the sentence on a single line
+
+Example:
+Enter a sentence: awesome possum
+mussop emosewa
+*/
+
+
