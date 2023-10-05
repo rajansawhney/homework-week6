31aad96cc4e419feed919bf498a82e1dd4461a17
rajansawhney <rajan.k.sawhney@gmail.com>
2023-10-04 20:42:07 -1000

    more comments

--- delete_element.c
+++ delete_element.c
@@ -4,7 +4,10 @@ Write a C program that deletes an element of a 1D array of a specific size at a 
 The size and elements of the array as well as the position of the element to deleted from the array are provided by the user.
 
 Note:
-1. Input integers for size of array, elements of array, and position from where an element needs to be deleted
+1. Input integers in the follwing order only:
+    - size of array
+    - elements of array
+    - position from where an element needs to be deleted
 2. Upon deletion of the element at the specified position, all elements after that position must be shifted one position to the left to fill up the gap.
 3. Display output as:
     Array after deletion: [<array elements>]
